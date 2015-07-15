561f9aab062e86d8973040a283d42b5fb51db5c9
Robert Djurasaj <robertd@users.noreply.github.com>
2015-07-15 14:20:03 -0600

    Update app.js

--- app/app.js
+++ app/app.js
@@ -14,7 +14,6 @@ var app = express();
 var mongoDbAddress = process.env.MONGO_PORT_27017_TCP_ADDR + ':' + process.env.MONGO_PORT_27017_TCP_PORT || 'localhost:27017';
 var connectionString = 'mongodb://' + mongoDbAddress + '/demoDB';
 console.log("MongoDb server running on: " + connectionString);
-console.log(process.env);
 mongoose.connect(connectionString);
 
 // view engine setup
