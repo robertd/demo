abf39d440b06679660c2264e94c1c66f37f78a91
Robert Djurasaj <robertd@users.noreply.github.com>
2015-07-15 14:26:51 -0600

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,12 +6,12 @@ var request = require('request');
 
 /* GET users page. */
 router.get('/', function(req, res, next) {
-	request('http://localhost:3000/api/users', function (error, response, body) {
-  	if (!error && response.statusCode == 200) {
-    	res.render('users', { users: body });
-  	}
-  	res.render('error');
-	});
+    request('http://localhost:3000/api/users', function (error, response, body) {
+      	if (!error && response.statusCode == 200) {
+        	res.render('users', { users: body });
+      	}
+      	res.render('error');
+    });
 });
 
 module.exports = router;
